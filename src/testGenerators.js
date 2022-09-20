37ec9094f1a80a69e3f354f0d20cc7211e5ddf3c
jhowlett-scottlogic <jhowlett@scottlogic.com>
2022-09-20 17:49:13 +0100

    add quotes

--- src/testGenerators.js
+++ src/testGenerators.js
@@ -26,8 +26,7 @@ function setupAndStartTests(generatorPath, arg1, arg2) {
 
     log.standard("Starting tests");
     console.log(`args: ${arg1} ${arg2}`)
-    // const test = shell.exec(`npm run test "${arg1}" "${arg2}"`, log.shellOptions);
-    const test = shell.exec(`npm run test ${arg1} ${arg2}`, log.shellOptions);
+    const test = shell.exec(`npm run test "${arg1}" "${arg2}"`, log.shellOptions);
 
     shell.cd(__dirname, log.shellOptions);
     return test.stdout.split("\n");
