f881c00337efb39b94d966112fddc42873055e45
jhowlett-scottlogic <jhowlett@scottlogic.com>
2022-09-21 11:31:03 +0100

    wd

--- src/testGenerators.js
+++ src/testGenerators.js
@@ -108,7 +108,7 @@ async function testGenerators(options) {
             
             console.log("featurePath:" + featurePath);
 
-            const stdout = setupAndStartTests(generatorPath, featurePath, "");
+            const stdout = setupAndStartTests(generatorPath, featurePath.replace("/", "\\"), "");
 
             const result = testResultParser.parseCSharp(stdout[stdout.length-2]);
 
